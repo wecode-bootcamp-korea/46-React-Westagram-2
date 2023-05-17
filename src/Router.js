a8f7677a8eedf2bec2e7a464e32b185a8aef5ac8
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-17 15:04:29 +0900

    초기 세팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Main from "./pages/Main/Main"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/jaewoong/Main';
 
-const Router = () =>{
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
