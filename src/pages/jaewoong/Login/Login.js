4d6935468c8a66c11a41f471190da650bd7d0d29
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-17 23:10:49 +0900

    ADD : files

--- src/pages/jaewoong/Login/Login.js
+++ src/pages/jaewoong/Login/Login.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import './Login.scss';
+import '../../../styles/common.scss';
+import { Link, useNavigate } from 'react-router-dom';
 
-const LoginJw = () => {
-  return <h1>Hello World</h1>;
-};
+function LoginJw() {
+  const navigate = useNavigate();
+  return (
+    <div className="container">
+      <div className="login-box">
+        <h1 className="font">Westagram</h1>
+
+        <form action="">
+          <input
+            type="text"
+            placeholder="전화번호,사용자 이름 또는 이메일"
+            id="id"
+            className=""
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            id="pw"
+            className="input"
+          />
+        </form>
+
+        <button
+          className="login-btn"
+          onClick={() => {
+            navigate('/main');
+          }}
+        >
+          로그인
+        </button>
+
+        <div className="repassword">
+          <a href="">비밀번호를 잊으셨나요?</a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LoginJw;
