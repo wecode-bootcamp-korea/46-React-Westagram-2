8539228a0d18cc5ab1002a4038c2c051dd4d35f2
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-19 02:54:20 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/gunwoong/Login/Login.js
+++ src/pages/gunwoong/Login/Login.js
@@ -4,21 +4,25 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const LoginGw = () => {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+  let [id, setId] = useState('');
+  let [pw, setPw] = useState('');
+
   const navigate = useNavigate();
 
   const goToMain = () => {
     navigate('/mainGw');
   };
 
-  const saveUserId = event => {
+  const savedUserId = event => {
     setId(event.target.value);
   };
 
   const savedUserPw = event => {
     setPw(event.target.value);
   };
+
+  const isActive = id.includes('@') && pw.length >= 5;
+
   return (
     <>
       <meta charSet="UTF-8" />
@@ -38,19 +42,20 @@ const LoginGw = () => {
             id="loginIdInput"
             type="text"
             placeholder="전화번호,사용자 이름 또는 이메일"
-            onChange={() => {
-              saveUserId();
-            }}
+            onChange={savedUserId}
           />
           <input
             id="loginPwInput"
             type="password"
             placeholder="비밀번호"
-            onChange={() => {
-              savedUserPw();
-            }}
+            onChange={savedUserPw}
           />
-          <button id="loginBtn" onClick={goToMain}>
+          <button
+            id="loginBtn"
+            onClick={goToMain}
+            disabled={!isActive}
+            className={isActive ? 'tomato' : 'orange'}
+          >
             로그인
           </button>
         </div>
