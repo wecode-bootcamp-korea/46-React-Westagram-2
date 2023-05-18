b0cdf1655f7efbca459b2ed3d4ab0cb1c7152f09
minjeong <hn01472@gmail.com>
2023-05-18 15:33:29 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/minjeong/Login/loginMj.js
+++ src/pages/minjeong/Login/loginMj.js
@@ -1,12 +1,25 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import './loginMj.scss';
 import { useNavigate } from 'react-router-dom';
 
 function LoginMj() {
   const navigate = useNavigate();
+  let [id, setId] = useState('');
+  let [pw, setPw] = useState('');
+
+  function saveUserId(evnet) {
+    setId(event.target.value);
+    console.log('id :', id);
+  }
+
+  function saveUserPw(evnet) {
+    setPw(event.target.value);
+    console.log('pw :', pw);
+  }
 
   const goToMain = () => {
+    saveUserId.inclu;
     navigate('/mainmj');
   };
 
@@ -18,9 +31,23 @@ function LoginMj() {
           className="inputId"
           type={'text'}
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={saveUserId}
         />
-        <input className="inputPw" type={'password'} placeholder="비밀번호" />
-        <button onClick={goToMain} className="disabled" type="button">
+        <input
+          className="inputPw"
+          type={'password'}
+          placeholder="비밀번호"
+          onChange={saveUserPw}
+        />
+        <button
+          onClick={e => {
+            setId(e.target.value).inclu('@') && setPw(e.target.value) > 5
+              ? button
+              : disable;
+          }}
+          className="disabled"
+          type="button"
+        >
           로그인
         </button>
         <a href="@@">비밀번호를 잊으셨나요?</a>
