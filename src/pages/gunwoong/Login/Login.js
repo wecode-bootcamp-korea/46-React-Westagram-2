27922b1625a801f9a1818e2384171755aecb0afb
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-25 21:46:08 +0900

    Fix: 리팩토링 input창 수정

--- src/pages/gunwoong/Login/Login.js
+++ src/pages/gunwoong/Login/Login.js
@@ -4,8 +4,12 @@ import '../../../styles/common.scss';
 import './Login.scss';
 
 const LoginGw = () => {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+  const [inputs, setInputs] = useState({
+    userId: '',
+    userPw: '',
+  });
+
+  const { userId, userPw } = inputs; //객체 구조분해할당해서 string값으로 사용할 수 있다. 안하면 inputs.userId 로 사용해야 돼
 
   const navigate = useNavigate();
 
@@ -13,21 +17,22 @@ const LoginGw = () => {
     navigate('/mainGw');
   };
 
-  const savedUserId = event => {
-    setId(event.target.value);
+  const onChange = e => {
+    //눌렀을 때 inputs value 값이 바뀌어야 돼
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
 
-  const savedUserPw = event => {
-    setPw(event.target.value);
-  };
-
-  const isActive = id.includes('@') && pw.length >= 5;
+  const isActive = userId.includes('@') && userPw.length >= 5;
 
   const goFetch = () => {
     fetch('http://10.58.52.110:3000/users/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({ email: id, password: pw }),
+      body: JSON.stringify({ email: userId, password: userPw }),
     }) //요청
       .then(res => res.json())
       .then(data => console.log(data));
@@ -41,17 +46,21 @@ const LoginGw = () => {
           id="loginIdInput"
           type="text"
           placeholder="전화번호,사용자 이름 또는 이메일"
-          onChange={savedUserId}
+          onChange={onChange}
+          name="userId"
+          value={userId}
         />
         <input
           id="loginPwInput"
           type="password"
           placeholder="비밀번호"
-          onChange={savedUserPw}
+          onChange={onChange}
+          name="userPw"
+          value={userPw}
         />
         <button
           id="loginBtn"
-          onClick={goFetch}
+          onClick={goToMain}
           disabled={!isActive}
           className={isActive ? 'tomato' : 'orange'}
         >
